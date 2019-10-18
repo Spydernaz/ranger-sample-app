75920459d22563a295f3a8f25c9270024840247f
Nathaniel Vala <nathanielvala@hotmail.com>
2019-10-18 09:41:22 +0100

    updated entities, tagsync code

--- source-code/httpservice/src/main/java/com/spyder/security/services/RangerServiceHTTP.java
+++ source-code/httpservice/src/main/java/com/spyder/security/services/RangerServiceHTTP.java
@@ -30,33 +30,25 @@ public class RangerServiceHTTP extends RangerBaseService {
 
     @Override
     public Map<String, Object> validateConfig() throws Exception {
-        if (LOG.isDebugEnabled()) {
-                LOG.debug("==> RangerServiceHTTP.validateConfig()");
-        }
+        LOG.info("==> RangerServiceHTTP.validateConfig()");
 
         Map<String, Object> ret = new HashMap<>();
 
         BaseClient.generateResponseDataMap(true, "ConnectionTest Successful", "ConnectionTest Successful", null, null, ret);
 
-        if (LOG.isDebugEnabled()) {
-                LOG.debug("<== RangerServiceAtlas.validateConfig(): " + ret );
-        }
+        LOG.info("<== RangerServiceAtlas.validateConfig(): " + ret );
 
         return ret;
     }
 
     @Override
     public List<String> lookupResource(ResourceLookupContext context)throws Exception {
-        if (LOG.isDebugEnabled()) {
-                LOG.debug("==> RangerServiceHTTP.lookupResource(" + context + ")");
-        }
+        LOG.info("==> RangerServiceHTTP.lookupResource(" + context + ")");
 
         // TODO: Get the list of routes from somewhere... maybe a /discovery route?
         List<String> ret = Arrays.asList("/", "/hello");
 
-        if (LOG.isDebugEnabled()) {
-                LOG.debug("<== RangerServiceAtlas.lookupResource("+ context + "): " + ret);
-        }
+        LOG.info("<== RangerServiceAtlas.lookupResource("+ context + "): " + ret);
 
         return ret;
     }
